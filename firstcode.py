9ba9d8956c9cee5541512bbca0597c505eb8bcb5
Hojjat <hojjat.salehinejad@gmail.com>
2016-10-31 11:47:10 -0400

    Adding firstcode.py

---
+++ firstcode.py
@@ -0,0 +1,18 @@
+from pyspark import SparkContext, SparkConf
+
+logFile = "README.md"  # Should be some file on your system
+conf=SparkConf()
+conf.setMaster("local[4]")
+conf.setAppName("Simple Khan")
+conf.set("spark.executor.memory", "1g")
+sc = SparkContext(conf=conf)
+
+logData = sc.textFile(logFile).cache()
+
+def counter(chri):
+    return 'a' in chri
+
+numAs = logData.filter(lambda s: 'a' in s).count()
+numBs = logData.filter(lambda s: 'b' in s).count()
+
+print "Lines with a: %i, lines with b: %i" % (numAs, numBs)
