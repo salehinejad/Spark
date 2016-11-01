d5b480c8a99d475910c9d97017a50fff134b9720
Hojjat <hojjat.salehinejad@gmail.com>
2016-11-01 15:15:11 -0400

    a practice on reduce

--- rdd_operations.py
+++ rdd_operations.py
@@ -13,6 +13,9 @@ logData = sc.textFile(logFile).cache()
 def summer(a,b):
     return a+b
 lineLengths = logData.map(lambda s: len(s)) # map performs the task
+lineLengths.persist() #to cache lineLenghts for later use - RDD persistence; to cancel: lineLengths.unpersist()
+# severl levels of persistence is possible - refer to manual
+
 #Aggregate the elements of the dataset using a function func
 totalLength1 = lineLengths.reduce(summer) # reduce collects the results; takes two returns one
 # methos 2
